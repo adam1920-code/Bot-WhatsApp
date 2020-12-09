3b2529f6036f7f58581487d6b7ad01c352dad550
TnDjarot-id404 <75694320+TnDjarot-id404@users.noreply.github.com>
2020-12-09 17:18:36 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,91 +4,91 @@ const options = require('./utils/options')
 const { color, messageLog } = require('./utils')
 const HandleMsg = require('./HandleMsg')
 
-const start = (djarot = new Client()) => {
+const start = (jarot = new Client()) => {
     console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
-    console.log(color(figlet.textSync('DJAROT BOT', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('ARUGA BOT', { font: 'Ghost', horizontalLayout: 'default' })))
     console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
-    console.log(color('[DEV]'), color('Djarot', 'yellow'))
+    console.log(color('[DEV]'), color('ArugaZ', 'yellow'))
     console.log(color('[~>>]'), color('BOT Started!', 'green'))
 
     // Mempertahankan sesi agar tetap nyala
-    djarot.onStateChanged((state) => {
+    jarot.onStateChanged((state) => {
         console.log(color('[~>>]', 'red'), state)
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') djarot.forceRefocus()
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') jarot.forceRefocus()
     })
 
     // ketika bot diinvite ke dalam group
-    djarot.onAddedToGroup(async (chat) => {
-	const groups = await djarot.getAllGroups()
+    jarot.onAddedToGroup(async (chat) => {
+	const groups = await jarot.getAllGroups()
 	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
 	if (groups.length > groupLimit) {
-	await djarot.sendText(chat.id, `Sorry, the group on this bot is full\nMax Group is: ${groupLimit}`).then(() => {
-	      djarot.leaveGroup(chat.id)
-	      djarot.deleteChat(chat.id)
+	await jarot.sendText(chat.id, `Sorry, the group on this bot is full\nMax Group is: ${groupLimit}`).then(() => {
+	      jarot.leaveGroup(chat.id)
+	      jarot.deleteChat(chat.id)
 	  }) 
 	} else {
 	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
 	    if (chat.groupMetadata.participants.length < memberLimit) {
-	    await djarot.sendText(chat.id, `Sorry, BOT comes out if the group members do not exceed ${memberLimit} people`).then(() => {
-	      djarot.leaveGroup(chat.id)
-	      djarot.deleteChat(chat.id)
+	    await jarot.sendText(chat.id, `Sorry, BOT comes out if the group members do not exceed ${memberLimit} people`).then(() => {
+	      jarot.leaveGroup(chat.id)
+	      jarot.deleteChat(chat.id)
 	    })
 	    } else {
-        await djarot.simulateTyping(chat.id, true).then(async () => {
-          await djarot.sendText(chat.id, `Hai minna~, Im Aruga BOT. To find out the commands on this bot type ${prefix}menu`)
+        await jarot.simulateTyping(chat.id, true).then(async () => {
+          await jarot.sendText(chat.id, `Hai minna~, Im Aruga BOT. To find out the commands on this bot type ${prefix}menu`)
         })
 	    }
 	}
     })
 
     // ketika seseorang masuk/keluar dari group
-    djarot.onGlobalParicipantsChanged(async (event) => {
-        const host = await djarot.getHostNumber() + '@c.us'
+    jarot.onGlobalParicipantsChanged(async (event) => {
+        const host = await jarot.getHostNumber() + '@c.us'
 		const welcome = JSON.parse(fs.readFileSync('./settings/welcome.json'))
 		const isWelcome = welcome.includes(event.chat)
-		let profile = await djarot.getProfilePicFromServer(event.who)
+		let profile = await jarot.getProfilePicFromServer(event.who)
 		if (profile == '' || profile == undefined) profile = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQcODjk7AcA4wb_9OLzoeAdpGwmkJqOYxEBA&usqp=CAU'
         // kondisi ketika seseorang diinvite/join group lewat link
         if (event.action === 'add' && event.who !== host && isWelcome) {
-			await djarot.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
-            await djarot.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
+			await jarot.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await jarot.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
         }
         // kondisi ketika seseorang dikick/keluar dari group
         if (event.action === 'remove' && event.who !== host) {
-			await djarot.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
-            await djarot.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
+			await jarot.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await jarot.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
         }
     })
 
-    djarot.onIncomingCall(async (callData) => {
+    jarot.onIncomingCall(async (callData) => {
         // ketika seseorang menelpon nomor bot akan mengirim pesan
-        await djarot.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
+        await jarot.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
         .then(async () => {
             // bot akan memblock nomor itu
-            await djarot.contactBlock(callData.peerJid)
+            await jarot.contactBlock(callData.peerJid)
         })
     })
 
     // ketika seseorang mengirim pesan
-    djarot.onMessage(async (message) => {
-        djarot.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+    jarot.onMessage(async (message) => {
+        jarot.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
             .then((msg) => {
                 if (msg >= 3000) {
-                    console.log('[djarot]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
-                    djarot.cutMsgCache()
+                    console.log('[jarot]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    jarot.cutMsgCache()
                 }
             })
-        HandleMsg(djarot, message)    
+        HandleMsg(jarot, message)    
     
     })
 	
     // Message log for analytic
-    djarot.onAnyMessage((anal) => { 
+    jarot.onAnyMessage((anal) => { 
         messageLog(anal.fromMe, anal.type)
     })
 }
 
 //create session
 create(options(true, start))
-    .then((djarot) => start(djarot))
+    .then((jarot) => start(jarot))
     .catch((err) => new Error(err))
